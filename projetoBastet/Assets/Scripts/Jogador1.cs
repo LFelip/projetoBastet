5d4555da8a825bb8592f53b483ace71f89bcfbc2
flurryy <71525318+flurryy-luc@users.noreply.github.com>
2020-09-25 20:45:57 -0300

    Add files via upload

---
+++ projetoBastet/Assets/Scripts/Jogador1.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador1 : MonoBehaviour
+{
+    
+    public float moveSpeed2;
+    public Rigidbody2D rigb2;
+    private Vector2 direcao2;
+    private bool troca2;
+    public float Speed2;
+    public float DistanciaPausa2;
+    private Transform Target2;
+
+
+    void Start()
+    {
+        Target2 = GameObject.FindGameObjectWithTag("Player2").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            troca2 = !troca2;
+        }
+
+       if(!troca2)
+        {
+            moveSpeed2 = 1f;
+
+            ProcessarInputs();
+            Habilidade();
+
+        }
+        
+        if(troca2)
+        {
+            moveSpeed2 = 0f; 
+
+            if(Vector2.Distance(transform.position, Target2.position) > DistanciaPausa2)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, Target2.position, Speed2 * Time.deltaTime);
+            } 
+      
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        Movimentar();
+    }
+
+    void ProcessarInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        direcao2 = new Vector2(moveX, moveY);
+    }
+
+    void Movimentar()
+    {
+        rigb2.velocity = new Vector2(direcao2.x * moveSpeed2, direcao2.y * moveSpeed2);
+    }
+
+    void Habilidade()
+    {
+
+    }
+
+}
---
+++ projetoBastet/Assets/Scripts/Jogador2.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador2 : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public Rigidbody2D rigb;
+    private Vector2 direcao;
+    private bool troca;
+    public float Speed;
+    public float DistanciaPausa;
+    private Transform Target;
+
+
+    void Start()
+    {
+        Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.F)) 
+        {
+            troca = !troca;
+        }
+
+       if(troca)
+        {
+            moveSpeed = 1f;
+
+            ProcessarInputs();
+            Habilidade();
+        }
+
+        if(!troca)
+        {
+            
+            moveSpeed = 0f;
+
+            if(Vector2.Distance(transform.position, Target.position) > DistanciaPausa)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, Target.position, Speed * Time.deltaTime);
+            }
+
+        }
+        
+
+    }
+
+    void FixedUpdate()
+    {
+        Movimentar();
+    }
+
+    void ProcessarInputs()
+    {
+        
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        direcao = new Vector2(moveX, moveY);
+
+        
+    }
+
+    void Movimentar()
+    {
+        rigb.velocity = new Vector2(direcao.x * moveSpeed, direcao.y * moveSpeed);
+
+    }
+
+    void Habilidade()
+    {
+
+    }
+
+}
