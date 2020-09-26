870843f15716f1e2654b81ca90af9fe6204df07a
Gigio <lfelipmn@gmail.com>
2020-09-26 18:02:19 -0300

    metadados

---
+++ projetoBastet/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    
+    public void StartGame () {
+        
+        SceneManager.LoadScene("oi");
+    }
+
+    public void QuitGame() {
+        
+        Application.Quit();
+    }
+}
