fcc6ab85098269627e9f18a1ce795b9e83c354ca
Gigio <lfelipmn@gmail.com>
2020-10-04 23:33:16 -0300

    a bunch of updates

---
+++ projetoBastet/Assets/Scripts/LevelController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelController : MonoBehaviour
+{
+    //Pra saber se tem os itens
+    private bool hasScepter = false;  
+    private bool hasEarRing = false;  
+    private bool hasAnk     = false;  
+
+    public Image[] itemSlots;
+
+    static LevelController instance;
+    
+    void Start()
+    {
+        if (instance == null )
+        {
+            instance = this;
+        }
+        
+    }
+
+    public GetItem(Sprite itemSprite)
+    {
+        foreach (Image itemSlot in itemSlots)
+        {
+            if (itemSlot.Sprite == null)
+            {
+                itemSlot.Sprite = itemSprite;
+                itemSlot.SetEnable(true);
+                break;
+            } 
+        }
+    }
+
+    
+}
